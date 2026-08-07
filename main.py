ff7ab941e6cad83fb491ed81aaea0176d1c142eb
Sean Coleman <bramblestar1.0@icloud.com>
2026-08-07 02:03:50 -0700

    import all stuff and modify to work with image

--- main.py
+++ main.py
@@ -2,69 +2,46 @@ import warnings
 warnings.filterwarnings("ignore")
 from torch import multiprocessing
 
-
 from collections import defaultdict
 
 import matplotlib.pyplot as plt
 import torch
 from tensordict.nn import TensorDictModule
 from tensordict.nn.distributions import NormalParamExtractor
-from torch import nn
-from torchrl.collectors import SyncDataCollector
+from torch import multiprocessing, nn
+
+from torchrl.collectors import Collector
 from torchrl.data.replay_buffers import ReplayBuffer
 from torchrl.data.replay_buffers.samplers import SamplerWithoutReplacement
 from torchrl.data.replay_buffers.storages import LazyTensorStorage
-from torchrl.envs import (Compose, DoubleToFloat, ObservationNorm, StepCounter,
-                          TransformedEnv)
+from torchrl.envs import (
+    Compose,
+    DoubleToFloat,
+    ToTensorImage,
+    StepCounter,
+    TransformedEnv,
+)
 from torchrl.envs.libs.gym import GymEnv
 from torchrl.envs.utils import check_env_specs, ExplorationType, set_exploration_type
 from torchrl.modules import ProbabilisticActor, TanhNormal, ValueOperator
 from torchrl.objectives import ClipPPOLoss
 from torchrl.objectives.value import GAE
 from tqdm import tqdm
-import os
 import gymnasium as gym
-import numpy as np
 
-device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
-torch.set_default_device(device=device)
-print(f"Using {device} device")
-
-Alpha = 1
-Horizon = 128
-Adam_stepsize = 2.5e-4 * Alpha
-Num_epochs = 3
-Minibatch_size = 32 * 8
-Discount = 0.99
-GAE_parameter = 0.95
-Number_of_actors = 8
-Clipping_parameter = 0.1 * Alpha
-
-state = torch.zeros((1,4,96,96), dtype=torch.float32, device=device)
-
-
-
-def preprocess(obs, prev_state):
-    #use ITU-R 601-2 luma formula to convert to grayscale
-    obs = np.dot(obs[..., :3], [0.299, 0.587, 0.114])
-
-
-    # obs = obs[..., np.newaxis]
-    obs = torch.tensor(obs, dtype=torch.float32, device=device).unsqueeze(0).unsqueeze(0)
-
-    state = torch.cat([prev_state, obs], dim=1)
-    if state.size(1) > 4:
-        state = state[:, 1:, ...]
-
-    return state
-
-
-        
-
-base_env = gym.make("CarRacing-v3", render_mode="human", lap_complete_percent=0.95, domain_randomize=False, continuous=False, max_episode_steps=1000)
-
+is_fork = multiprocessing.get_start_method() == "fork"
+device = (
+    torch.device(0)
+    if torch.cuda.is_available() and not is_fork
+    else torch.device("cpu")
+)
+torch.set_default_device(device)
+num_cells = 256  # number of cells in each layer i.e. output dim.
+lr = 3e-4
+max_grad_norm = 1.0
 
 frames_per_batch = 1000
+# For a complete training, bring the number of frames up to 1M
 total_frames = 500000
 
 sub_batch_size = 64  # cardinality of the sub-samples gathered from the current data in the inner loop
@@ -72,35 +49,103 @@ num_epochs = 10  # optimization steps per batch of data collected
 clip_epsilon = (
     0.2  # clip value for PPO loss: see the equation in the intro for more context.
 )
-gamma = Discount
-lmbda = GAE_parameter
+gamma = 0.99
+lmbda = 0.95
 entropy_eps = 1e-4
+
+base_env = GymEnv("CarRacing-v3", render_mode="human", lap_complete_percent=0.95, domain_randomize=True, continuous=False)
+
 
 env = TransformedEnv(
     base_env,
     Compose(
         # normalize observations
-        ObservationNorm(in_keys=["observation"]),
-        DoubleToFloat(),
+        ToTensorImage(in_keys=["pixels"]), #use to tensor image instead bc its an image, not some data abt the position
         StepCounter(),
     ),
 )
 
+check_env_specs(env)
 
-    # running_reward.append(total_reward)
-    # ep_reward.append(sum(running_reward)/len(running_reward))
-    # plt.plot(range(len(ep_reward)), ep_reward)
-    # plt.title("Episode Rewards")
-    # plt.xlabel("Time")
-    # plt.ylabel("Rewards")
-    # plt.savefig('.plots/reward_plot.png')
-    # plt.close()  
+# print("action_spec (as defined by input_spec):", env.action_spec.shape[-1])
 
-    # plt.plot(range(len(train_loss)), train_loss)
-    # plt.title("Training Loss")
-    # plt.xlabel("Time")
-    # plt.ylabel("Loss") 
-    # plt.savefig('.plots/train_loss.png')
-    # plt.close() 
+actor_net = nn.Sequential(
+            nn.LazyConv2d(16, kernel_size=12, stride=4), 
+            nn.Tanh(),
+            nn.LazyConv2d(32, kernel_size=4, stride=2), 
+            nn.Tanh(),
+            nn.Flatten(),
+            nn.LazyLinear(256),
+            nn.Tanh(),
+            nn.LazyLinear(2 * env.action_spec.shape[-1], device=device),
+            NormalParamExtractor()
+        )
 
-torch.save(policy_model, "policymodel.pth")
+policy_module = TensorDictModule(
+    actor_net, in_keys=["pixels"], out_keys=["loc", "scale"]
+)
+
+policy_module = ProbabilisticActor(
+    module=policy_module,
+    spec=env.action_spec,
+    in_keys=["loc", "scale"],
+    distribution_class=TanhNormal,
+    # distribution_kwargs={
+    #     "low": env.action_spec_unbatched.space.low,
+    #     "high": env.action_spec_unbatched.space.high,
+    # },
+    return_log_prob=True,
+)
+
+value_net = nn.Sequential(
+            nn.LazyConv2d(16, kernel_size=12, stride=4), 
+            nn.Tanh(),
+            nn.LazyConv2d(32, kernel_size=4, stride=2), 
+            nn.Tanh(),
+            nn.Flatten(),
+            nn.LazyLinear(256),
+            nn.Tanh(),
+            nn.LazyLinear(1, device=device)
+        )
+
+value_module = ValueOperator(
+    module=value_net,
+    in_keys=["pixels"],
+)
+
+print("Running policy:", policy_module(env.reset()))
+print("Running value:", value_module(env.reset()))
+
+collector = Collector(
+    env,
+    policy_module,
+    frames_per_batch=frames_per_batch,
+    total_frames=total_frames,
+    split_trajs=False,
+    device=device,
+)
+
+replay_buffer = ReplayBuffer(
+    storage=LazyTensorStorage(max_size=frames_per_batch),
+    sampler=SamplerWithoutReplacement(),
+)
+
+advantage_module = GAE(
+    gamma=gamma, lmbda=lmbda, value_network=value_module, average_gae=True
+)
+
+loss_module = ClipPPOLoss(
+    actor_network=policy_module,
+    critic_network=value_module,
+    clip_epsilon=clip_epsilon,
+    entropy_bonus=bool(entropy_eps),
+    entropy_coeff=entropy_eps,
+    # these keys match by default but we set this for completeness
+    critic_coeff=1.0,
+    loss_critic_type="smooth_l1",
+)
+
+optim = torch.optim.Adam(loss_module.parameters(), lr)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+    optim, total_frames // frames_per_batch, 0.0
+)
