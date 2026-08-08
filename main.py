45c8782008273ea6fa9ccd31e3b705081d773e73
Sean Coleman <bramblestar1.0@icloud.com>
2026-08-07 19:55:46 -0700

    works

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import warnings
 warnings.filterwarnings("ignore")
 from torch import multiprocessing
@@ -53,7 +54,7 @@ gamma = 0.99
 lmbda = 0.95
 entropy_eps = 1e-4
 
-base_env = GymEnv("CarRacing-v3", render_mode="human", lap_complete_percent=0.95, domain_randomize=True, continuous=False)
+base_env = GymEnv("CarRacing-v3", render_mode="human", lap_complete_percent=0.95, domain_randomize=False, continuous=True, device=device)
 
 
 env = TransformedEnv(
@@ -74,7 +75,7 @@ actor_net = nn.Sequential(
             nn.Tanh(),
             nn.LazyConv2d(32, kernel_size=4, stride=2), 
             nn.Tanh(),
-            nn.Flatten(),
+            nn.Flatten(start_dim=-3, end_dim=-1),
             nn.LazyLinear(256),
             nn.Tanh(),
             nn.LazyLinear(2 * env.action_spec.shape[-1], device=device),
@@ -102,7 +103,7 @@ value_net = nn.Sequential(
             nn.Tanh(),
             nn.LazyConv2d(32, kernel_size=4, stride=2), 
             nn.Tanh(),
-            nn.Flatten(),
+            nn.Flatten(start_dim=-3, end_dim=-1),
             nn.LazyLinear(256),
             nn.Tanh(),
             nn.LazyLinear(1, device=device)
@@ -113,8 +114,9 @@ value_module = ValueOperator(
     in_keys=["pixels"],
 )
 
-print("Running policy:", policy_module(env.reset()))
+print("Running policy:", policy_module(env.reset())["action"].shape)
 print("Running value:", value_module(env.reset()))
+
 
 collector = Collector(
     env,
@@ -149,3 +151,88 @@ optim = torch.optim.Adam(loss_module.parameters(), lr)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
     optim, total_frames // frames_per_batch, 0.0
 )
+
+logs = defaultdict(list)
+pbar = tqdm(total=total_frames)
+eval_str = ""
+
+# We iterate over the collector until it reaches the total number of frames it was
+# designed to collect:
+for i, tensordict_data in enumerate(collector):
+    # we now have a batch of data to work with. Let's learn something from it.
+    for _ in range(num_epochs):
+        # We'll need an "advantage" signal to make PPO work.
+        # We re-compute it at each epoch as its value depends on the value
+        # network which is updated in the inner loop.
+        advantage_module(tensordict_data)
+        data_view = tensordict_data.reshape(-1)
+        replay_buffer.extend(data_view.cpu())
+        for _ in range(frames_per_batch // sub_batch_size):
+            subdata = replay_buffer.sample(sub_batch_size)
+            loss_vals = loss_module(subdata.to(device))
+            loss_value = (
+                loss_vals["loss_objective"]
+                + loss_vals["loss_critic"]
+                + loss_vals["loss_entropy"]
+            )
+
+            # Optimization: backward, grad clipping and optimization step
+            loss_value.backward()
+            # this is not strictly mandatory but it's good practice to keep
+            # your gradient norm bounded
+            torch.nn.utils.clip_grad_norm_(loss_module.parameters(), max_grad_norm)
+            optim.step()
+            optim.zero_grad()
+
+    logs["reward"].append(tensordict_data["next", "reward"].mean().item())
+    pbar.update(tensordict_data.numel())
+    cum_reward_str = (
+        f"average reward={logs['reward'][-1]: 4.4f} (init={logs['reward'][0]: 4.4f})"
+    )
+    logs["step_count"].append(tensordict_data["step_count"].max().item())
+    stepcount_str = f"step count (max): {logs['step_count'][-1]}"
+    logs["lr"].append(optim.param_groups[0]["lr"])
+    lr_str = f"lr policy: {logs['lr'][-1]: 4.4f}"
+    if i % 10 == 0:
+        # We evaluate the policy once every 10 batches of data.
+        # Evaluation is rather simple: execute the policy without exploration
+        # (take the expected value of the action distribution) for a given
+        # number of steps (1000, which is our ``env`` horizon).
+        # The ``rollout`` method of the ``env`` can take a policy as argument:
+        # it will then execute this policy at each step.
+        with set_exploration_type(ExplorationType.DETERMINISTIC), torch.no_grad():
+            # execute a rollout with the trained policy
+            eval_rollout = env.rollout(1000, policy_module)
+            logs["eval reward"].append(eval_rollout["next", "reward"].mean().item())
+            logs["eval reward (sum)"].append(
+                eval_rollout["next", "reward"].sum().item()
+            )
+            logs["eval step_count"].append(eval_rollout["step_count"].max().item())
+            eval_str = (
+                f"eval cumulative reward: {logs['eval reward (sum)'][-1]: 4.4f} "
+                f"(init: {logs['eval reward (sum)'][0]: 4.4f}), "
+                f"eval step-count: {logs['eval step_count'][-1]}"
+            )
+            del eval_rollout
+    pbar.set_description(", ".join([eval_str, cum_reward_str, stepcount_str, lr_str]))
+
+    # We're also using a learning rate scheduler. Like the gradient clipping,
+    # this is a nice-to-have but nothing necessary for PPO to work.
+    scheduler.step()
+    for filename in os.listdir('.plots'):
+        os.remove(os.path.join('.plots', filename))
+    plt.figure(figsize=(10, 10))
+    plt.subplot(2, 2, 1)
+    plt.plot(logs["reward"])
+    plt.title("training rewards (average)")
+    plt.subplot(2, 2, 2)
+    plt.plot(logs["step_count"])
+    plt.title("Max step count (training)")
+    plt.subplot(2, 2, 3)
+    plt.plot(logs["eval reward (sum)"])
+    plt.title("Return (test)")
+    plt.subplot(2, 2, 4)
+    plt.plot(logs["eval step_count"])
+    plt.title("Max step count (test)")
+    plt.savefig('.plots/plot.png')
+    plt.close() 
